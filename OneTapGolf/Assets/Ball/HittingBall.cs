6bfd2dc5035e5e9e549dcd6169736313ff30dc94
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-02 21:31:18 +0100

    Dodane uderzanie piłeczki, jeszcze nie działa jak należy

---
+++ OneTapGolf/Assets/Ball/HittingBall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittingBall : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject objectBall;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Hit();
+        }
+	}
+
+    void Hit()
+    {
+        Instantiate(objectBall, firePoint.position, firePoint.rotation);
+    }
+}
