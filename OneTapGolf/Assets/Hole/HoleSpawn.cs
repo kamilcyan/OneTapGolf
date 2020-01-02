b29aafe2765a9712bcad6b54f25db168cf7b6119
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-02 20:48:53 +0100

    Pierwszy commit, dodane obiekty gry, background, generowanie dołków

---
+++ OneTapGolf/Assets/Hole/HoleSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleSpawn : MonoBehaviour {
+
+    public GameObject objectHole;
+    public float spawnX = 6f;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        float random = Random.Range(-spawnX, spawnX);
+        Vector3 spawnPos = transform.position + new Vector3(random, 0f, 0f);
+        Instantiate(objectHole, spawnPos, Quaternion.identity);
+
+        Invoke("Spawn", Random.Range(-1, 10));
+    }
+}
---
+++ OneTapGolf/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Text gameOverText;
+
+    int playerScore = 0;
+
+    public void AddScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void PlayerLost()
+    {
+        gameOverText.enabled = true;
+        Time.timeScale = 0;
+    }
+}
